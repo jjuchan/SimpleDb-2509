2a3981061b5b0ea8076c7e9c4fc75dab390c2782
jjuchan <yjc7241@naver.com>
2025-09-15 16:33:06 +0900

    chore:setup

--- src/main/java/com/back/simpleDb/SimpleDb.java
+++ src/main/java/com/back/simpleDb/SimpleDb.java
@@ -1,4 +1,32 @@
 package com.back.simpleDb;
 
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class SimpleDb {
+
+    private Connection connection;
+    private boolean devMode;
+
+
+    @SneakyThrows
+    public SimpleDb(String host, String username, String password, String dbName) {
+        this.connection = DriverManager.getConnection(
+                "jdbc:mysql://" + host + ":3306/" + dbName, username, password
+        );
+    }
+
+    public void setDevMode(boolean devMode) {
+        this.devMode = devMode;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void run(String sql, Object... params ) {
+
+    }
 }
